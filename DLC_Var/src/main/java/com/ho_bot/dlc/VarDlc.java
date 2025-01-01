6a62078ea6336c8a26d1e8c1a99bb22d71c0d1be
HoBot06 <64059883+HoBot06@users.noreply.github.com>
2025-01-01 14:39:31 +0900

    v0.5

--- DLC_Var/src/main/java/com/ho_bot/dlc/VarDlc.java
+++ DLC_Var/src/main/java/com/ho_bot/dlc/VarDlc.java
@@ -1,23 +1,20 @@
 package com.ho_bot.dlc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import com.ho_bot.main.DLC_Var;
+import com.ho_bot.util.DLCUtil;
 import com.ho_bot.util.VarUtil;
 
 public class VarDlc extends DLC_Donation{
 	
-	private Random rand = new Random();
+	//private Random rand = new Random();
 
 	public VarDlc(String key) {
 		super(key);
@@ -26,13 +23,103 @@ public class VarDlc extends DLC_Donation{
 	@Override
 	public void active(UUID uuid, String playername, String sender, String platform, int cash, String cmd) {
 		String[] cmd_s = cmd.split(" ");
-		new BukkitRunnable() { 
-			
-			@Override
-			public void run() {
-				Bukkit.getPlayer(uuid).sendMessage(cmd_s+"");
+		if(cmd_s[0].equalsIgnoreCase(":isop")) {
+			isOp(uuid, playername, sender, platform, cash, cmd);
+		}
+		
+	}
+	
+	private void isOp(UUID uuid, String playername, String sender, String platform, int cash, String cmd) {
+		if(!Bukkit.getPlayer(uuid).isOp()) return;
+		Map<String, DLC_Donation> dlc_list = DLCUtil.getDlclist();
+		String s_cmd = cmd.replace(":isop ", "");
+		String[] ho_cmd = s_cmd.split(":");
+		if(ho_cmd[0].equalsIgnoreCase("honation")) {
+			if(ho_cmd.length >= 2) {
+				try {
+					if(ho_cmd[1].equalsIgnoreCase("kill")) {
+						final Player player = Bukkit.getPlayer(uuid);
+						player.setHealth(0);
+						return;
+					}
+					if(ho_cmd[1].equalsIgnoreCase("alltitle")) {
+						String title = "";
+						String subTitle = "";
+						int fadeIn = 0;
+						int stay = 0;
+						int fadeOut = 0;
+						for(int i = 2; i < ho_cmd.length; i++) {
+							if(i==2) {
+								title = ho_cmd[i].replace('&', ChatColor.COLOR_CHAR);
+							}
+							if(i==3) {
+								subTitle = ho_cmd[i].replace('&', ChatColor.COLOR_CHAR);
+							}
+							if(i==4) {
+								fadeIn = Integer.parseInt(ho_cmd[i]);
+							}
+							if(i==5) {
+								stay = Integer.parseInt(ho_cmd[i]);
+							}
+							if(i==6) {
+								fadeOut = Integer.parseInt(ho_cmd[i]);
+							}
+						}
+						for(Player allp : Bukkit.getOnlinePlayers()) {
+							allp.sendTitle(title, subTitle, fadeIn, stay, fadeOut);
+						}
+						return;
+					}
+					if(ho_cmd[1].equalsIgnoreCase("title")) {
+						Player player = Bukkit.getPlayer(uuid);
+						String title = "";
+						String subTitle = "";
+						int fadeIn = 0;
+						int stay = 0;
+						int fadeOut = 0;
+						for(int i = 2; i < ho_cmd.length; i++) {
+							if(i==2) {
+								title = ho_cmd[i].replace('&', ChatColor.COLOR_CHAR);
+							}
+							if(i==3) {
+								subTitle = ho_cmd[i].replace('&', ChatColor.COLOR_CHAR);
+							}
+							if(i==4) {
+								fadeIn = Integer.parseInt(ho_cmd[i]);
+							}
+							if(i==5) {
+								stay = Integer.parseInt(ho_cmd[i]);
+							}
+							if(i==6) {
+								fadeOut = Integer.parseInt(ho_cmd[i]);
+							}
+						}
+						player.sendTitle(title, subTitle, fadeIn, stay, fadeOut);
+						return;
+					}
+					if(ho_cmd[1].equalsIgnoreCase("teleport")) {
+						Player player = Bukkit.getPlayer(uuid);
+						String world = ho_cmd[2];
+						double x = Double.parseDouble(ho_cmd[3]);
+						double y = Double.parseDouble(ho_cmd[4]);
+						double z = Double.parseDouble(ho_cmd[5]);
+						Location loc = new Location(Bukkit.getWorld(world), x, y, z);
+						player.teleport(loc);
+						return;
+					}
+					if(VarUtil.dlclist.containsKey(ho_cmd[1])) {
+						String dlc_cmd = s_cmd.replace("honation:"+ho_cmd[1], "");
+						dlc_list.get(ho_cmd[1]).active(uuid, playername, sender, platform, cash, dlc_cmd);
+						return;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}.runTask(DLC_Var.inst);
+		}
+		else {
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s_cmd);
+		}
 	}
 
 
--- DLC_Var/src/main/java/com/ho_bot/main/DLC_Var.java
+++ DLC_Var/src/main/java/com/ho_bot/main/DLC_Var.java
@@ -2,7 +2,6 @@ package com.ho_bot.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 import com.ho_bot.dlc.VarDlc;
 import com.ho_bot.util.DLCUtil;
@@ -10,14 +9,12 @@ import com.ho_bot.util.DLCUtil;
 public class DLC_Var extends JavaPlugin {
 	
 	public static DLC_Var inst;
-	public static ScoreboardManager scoreManager;
 	
-	private final String key = "DLC_Var";
+	private final String key = "Var";
 	
 	@Override
 	public void onEnable() {
 		inst = this;
-		scoreManager = Bukkit.getScoreboardManager();
 		Bukkit.getConsoleSender().sendMessage("DLC_Var " + getDescription().getVersion() + " Online");
         
         getConfig().options().copyDefaults(true);
